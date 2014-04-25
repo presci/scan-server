c1fd3320962172a3815c25b4651205c2e3cab622
cbar <c.bar@mail.ru>
2014-04-25 16:08:23 -0700

    Added dir.html and json handler

--- scanserver.py
+++ scanserver.py
@@ -4,16 +4,17 @@ import os
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from os import curdir, sep
 import ConfigParser
+import urlparse
+import json
+from StringIO import StringIO
+import os
+
 
 PORT_NUMBER = 9093
 SCANCOMMAND="/home/prasad/Workspace/scan-server/scanimage.sh"
 SCANSTOP="kill $(ps aux | grep scanimage | grep device | awk '{print $2}')"
 
 class MyHandler(BaseHTTPRequestHandler):
-    def __init__(self):
-        super(MyHandler, sefl).__init__()
-        self.config = ConfigParser.ConfigParser()
-        self.config.read('scanserver.ini')
     def serve(self, path, restype='text/html'):
         f=open('static' + path)
         self.send_response(200)
@@ -22,6 +23,30 @@ class MyHandler(BaseHTTPRequestHandler):
         self.wfile.write(f.read())
         f.close()
         return
+    def dumpjson(self, data):
+        io = StringIO()
+        json.dump(data, io)
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(io.getvalue())
+        return
+    def handlerequest(self):
+        K=urlparse.urlparse(self.path)
+        if K.path.endswith('token'):
+            query=dict(urlparse.parse_qsl(K.query))
+            self.code=query['code']
+            self.serve('/dir.html')
+            return False
+        if K.path.endswith('dir.json'):
+            count = 5
+            if K.query != '':
+                query=dict(urlparse.parse_qsl(K.query))
+                count=int(query['count'])
+            dirs=[d for d in os.listdir('/Users/piyer/windows/workspace/') if os.path.isdir(os.path.join('/Users/piyer/windows/workspace/', d))]
+            d=sorted(dirs, key=lambda x: os.path.getctime(), reverse=True)[:count]
+            self.dumpjson(d)
+            return False
     def do_GET(self):
         print self.path
         if self.path.endswith('html'):
@@ -32,25 +57,24 @@ class MyHandler(BaseHTTPRequestHandler):
             return
         try:
             sendReply = False
-            if self.path.endswith('token'):
-                print self.path
-                return
             if self.path.endswith("scan"):
                 print "found request"
                 os.system(SCANCOMMAND)
                 sendReply = True
-            elif self.path.endswith('scanstop'):
+            if self.path.endswith('scanstop'):
                 print 'stopping scan'
                 os.system(SCANSTOP)
                 sendReply = True
+            if self.path.endswith('test'):
+                sendReply = True
+            else :
+                sendReply=self.handlerequest()
             if sendReply == True:
                 self.send_response(204)
                 self.end_headers()
             return
         except IOError:
             self.send_error(404, 'File not found %s' % self.path)
-
-
 try:
     server = HTTPServer(('', PORT_NUMBER), MyHandler)
     print "Started Server at port ", PORT_NUMBER
--- static/leaf.js
+++ static/leaf.js
@@ -1,11 +1,40 @@
 var a = (function(){
 	var init=function(){
 	    $('#login').click(function(){
-		    var myuri="https://accounts.google.com/o/oauth2/auth?scope="+encodeURIComponent('')+ "&redirect_uri="+encodeURIComponent('http://localhost:9093/token')+"&response_type=code&client_id=";
-
-		    window.open(myuri,"_blank","menubar=no,toolbar=no, scrollbars=no, resizable=no, top=500, left=500, width=400, height=400");
+		    var myuri="https://accounts.google.com/o/oauth2/auth?scope="+encodeURIComponent('https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.file')+ "&redirect_uri="+encodeURIComponent('http://localhost:9093/token')+"&response_type=code&client_id=284508695390-gotsg7835gar48pr41g3o5qi5i0f92t5.apps.googleusercontent.com";
+		    window.location.assign(myuri);
 		});
-	}
+	    $('#startscan').click(function(){
+		    $.ajax({
+			    url:'http://localhost:9093/scan',
+				beforeSend:function(xhr){
+				xhr.overrideMimeType('text/plain; charset=x-user-defined');
+			    }}).done(function(data, textstatus, jq){
+				    console.log(jq);
+				});
+		});
+	    $('#stopscan').click(function(){
+		    $.ajax({
+			    url:'http://localhost:9093/scan',
+				beforeSend:function(xhr){
+				xhr.overrideMimeType('text/plain; charset=x-user-defined');
+			    }}).done(function(data, textstatus, jq){
+				    console.log(jq);
+				});
+		});
+	    $('#test').click(function(){
+		    $.ajax({
+			    url:'http://localhost:9093/test',
+				beforeSend:function(xhr){
+				xhr.overrideMimeType('text/plain; charset=x-user-defined');
+			    }}).done(function(data, textstatus, jq){
+				    console.log(jq);
+				});
+		});
+	    
 
+	};
+	
+	
 	init();
     })();  
